944a27e50c9dc0c11ad55841b81a1e35f4a25f99
Daniel Cousens <dcousens@users.noreply.github.com>
2015-03-25 00:28:26 +1100

    index: remove unneccesary comment

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// overrides provided fields recursively with their equivalent: no-space uppercase names if they exist in process.env
 module.exports = function divulge(obj, prefix) {
   for (var key in obj) {
     var envKey = prefix + key.toUpperCase()
